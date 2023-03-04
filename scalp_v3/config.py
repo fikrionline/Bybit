878dc3a0b4120cf8c0754b42d812a75cb4f01c3a
Fikri Pasti Sukses <fikrionline@gmail.com>
2023-03-04 16:35:24 +0700

    Create config.py

---
+++ scalp_v3/config.py
@@ -0,0 +1,10 @@
+# Bybit API keys
+api_key = 'Kw8qXWsRhhwYBUqBuN'
+api_secret = 'huGDTCtuxTPUO9s6AU3PKr1XAqtwD6viJf3N'
+
+# Binance API Keys
+binance_api_key = 'hSKoPlxAG6NlOOMykSgh7XeHFNhf0Mz6FWgo7Z7phjVib9WNp9Wig0tssmwrZFqr'
+binance_api_secret = '9Pb7GbpAmCaGlsIayX4W2HKXQqL8Ik15kBDFTxCnhJ1r50IlNjzX76VjrsF3Cs9T'
+
+endpoint = 'https://api.bybit.com'
+domain = 'bybit'
